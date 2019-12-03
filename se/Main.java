f8bef25cf41ba9a8f31d2afe25a06de4ac257f0f
Yewonn <kimyw.vip@gmail.com>
2019-12-03 16:07:09 +0900

    Update Main.java

--- se/Main.java
+++ se/Main.java
@@ -3,6 +3,7 @@ package se;
 // 1: remote by Yewonnn
 // 1: local by Yewonnn
 // 2: remote by Yewonnn
+// 1: localBranch01 by Yewonnn
 public class Main {
 
 	public static void main(String[] args) {
